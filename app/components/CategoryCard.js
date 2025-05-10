91fcfe998e4faf435f034107faa0578c3c35203f
andrewmillen <andrewmillen1@gmail.com>
2025-05-09 22:57:16 -0500

    Initial commit

---
+++ app/components/CategoryCard.js
@@ -0,0 +1,40 @@
+"use client";
+
+import { ArrowPathIcon } from "@heroicons/react/24/outline";
+
+export default function CategoryCard({
+  title,
+  selected,
+  isSpinning,
+  isSpinningCategory,
+  onSpin,
+  color,
+}) {
+  return (
+    <div className="bg-white dark:bg-black p-6 text-center w-full max-w-sm border border-neutral-700 dark:border-neutral-100">
+      <h2 className="text-sm mb-2 text-neutral-800 dark:text-neutral-500 uppercase tracking-widest">
+        {title}
+      </h2>
+      <div
+        className={`text-2xl mb-4 ${
+          isSpinning || isSpinningCategory ? "text-neutral-400" : color
+        }`}
+      >
+        {selected}
+      </div>
+      <button
+        onClick={onSpin}
+        disabled={isSpinning || isSpinningCategory}
+        className={`p-3 rounded-full transform ${
+          isSpinning || isSpinningCategory
+            ? "bg-neutral-100 text-neutral-400 dark:bg-neutral-900 dark:text-neutral-500 cursor-not-allowed"
+            : "bg-neutral-200 text-neutral-800 dark:bg-neutral-800 dark:text-white hover:bg-neutral-300 hover:dark:bg-neutral-700 hover:cursor-pointer"
+        }`}
+      >
+        <ArrowPathIcon
+          className={`w-5 h-5 ${isSpinningCategory ? "animate-spin" : ""}`}
+        />
+      </button>
+    </div>
+  );
+}
--- app/page.js
+++ app/page.js
@@ -1,103 +1,180 @@
-import Image from "next/image";
+"use client";
+
+import {
+  ArrowPathIcon,
+  MagnifyingGlassIcon,
+} from "@heroicons/react/24/outline";
+
+import CategoryCard from "./components/CategoryCard";
+import { useState } from "react";
+
+const proteins = [
+  "Chicken Breast",
+  "Chicken Thigh",
+  "Chicken Wing",
+  "Shrimp",
+  "Salmon",
+  "Pork Chop",
+  "Pork Tenderloin",
+  "Ground Pork",
+  "Ground Beef",
+  "Steak",
+  "Bratwurst",
+  "Ground Sausage",
+  "Sausage",
+];
+
+const starches = ["Potatoes", "Pasta", "Rice", "Grits"];
+
+const cuisines = [
+  "American",
+  "Mexican",
+  "Indian",
+  "Mediterranean",
+  "Middle Eastern",
+  "Irish",
+  "Asian",
+  "Italian",
+  "Cajun",
+  "Creole",
+  "German",
+];
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              app/page.js
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+  const [isSpinning, setIsSpinning] = useState(false);
+  const [isSpinningProtein, setIsSpinningProtein] = useState(false);
+  const [isSpinningStarch, setIsSpinningStarch] = useState(false);
+  const [isSpinningCuisine, setIsSpinningCuisine] = useState(false);
+  const [selectedProtein, setSelectedProtein] = useState(proteins[0]);
+  const [selectedStarch, setSelectedStarch] = useState(starches[0]);
+  const [selectedCuisine, setSelectedCuisine] = useState(cuisines[0]);
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+  const getRandomItem = (array) => {
+    return array[Math.floor(Math.random() * array.length)];
+  };
+
+  const spinSingle = (setIsSpinning, setSelected, array) => {
+    setIsSpinning(true);
+    let spins = 0;
+    const maxSpins = 20;
+    const interval = setInterval(() => {
+      setSelected(getRandomItem(array));
+      spins++;
+      if (spins >= maxSpins) {
+        clearInterval(interval);
+        setIsSpinning(false);
+      }
+    }, 100);
+  };
+
+  const spin = () => {
+    setIsSpinning(true);
+    let spins = 0;
+    const maxSpins = 20;
+    const interval = setInterval(() => {
+      setSelectedProtein(getRandomItem(proteins));
+      setSelectedStarch(getRandomItem(starches));
+      setSelectedCuisine(getRandomItem(cuisines));
+
+      spins++;
+      if (spins >= maxSpins) {
+        clearInterval(interval);
+        setIsSpinning(false);
+      }
+    }, 100);
+  };
+
+  return (
+    <div className="min-h-screen p-8 flex flex-col items-center md:justify-center">
+      <main className="w-full max-w-5xl mx-auto">
+        <h1 className="text-3xl lg:text-4xl text-center mb-8 lg:mb-12 text-neutral-700 dark:text-white">
+          Dinner Configurator 9000
+        </h1>
+
+        <div className="flex flex-col lg:flex-row space-y-4 lg:space-y-0 lg:space-x-8 items-center">
+          <CategoryCard
+            title="Protein"
+            selected={selectedProtein}
+            isSpinning={isSpinning}
+            isSpinningCategory={isSpinningProtein}
+            onSpin={() =>
+              spinSingle(setIsSpinningProtein, setSelectedProtein, proteins)
+            }
+            color="text-amber-600 dark:text-amber-500"
+          />
+
+          <CategoryCard
+            title="Starch"
+            selected={selectedStarch}
+            isSpinning={isSpinning}
+            isSpinningCategory={isSpinningStarch}
+            onSpin={() =>
+              spinSingle(setIsSpinningStarch, setSelectedStarch, starches)
+            }
+            color="text-sky-600 dark:text-sky-400"
+          />
+
+          <CategoryCard
+            title="Cuisine"
+            selected={selectedCuisine}
+            isSpinning={isSpinning}
+            isSpinningCategory={isSpinningCuisine}
+            onSpin={() =>
+              spinSingle(setIsSpinningCuisine, setSelectedCuisine, cuisines)
+            }
+            color="text-teal-600 dark:text-teal-400"
+          />
+        </div>
+
+        <div className="text-center mt-8 lg:mt-12">
+          <button
+            onClick={spin}
+            disabled={
+              isSpinning ||
+              isSpinningProtein ||
+              isSpinningStarch ||
+              isSpinningCuisine
+            }
+            className={`pl-7 pr-8 py-4 text-lg md:text-xl rounded-full transform inline-flex items-center space-x-3 ${
+              isSpinning ||
+              isSpinningProtein ||
+              isSpinningStarch ||
+              isSpinningCuisine
+                ? "bg-neutral-100 text-neutral-400 dark:bg-neutral-900 dark:text-neutral-500 cursor-not-allowed"
+                : "bg-neutral-800 text-white dark:bg-white dark:text-neutral-900 hover:cursor-pointer hover:bg-neutral-700 dark:hover:bg-neutral-300"
+            }`}
           >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+            {isSpinning ? (
+              <>
+                <ArrowPathIcon className="w-5 h-5 inline-block mr-3 animate-spin" />
+                Spin All Categories
+              </>
+            ) : (
+              <>
+                <ArrowPathIcon className="w-5 h-5 inline-block mr-3" />
+                Spin All Categories
+              </>
+            )}
+          </button>
+          <div className="mt-4">
+            <button
+              onClick={() => {
+                const result = `${selectedCuisine} ${selectedProtein} and ${selectedStarch} recipe`;
+                window.open(
+                  `https://www.google.com/search?q=${encodeURIComponent(
+                    result
+                  )}`,
+                  "_blank"
+                );
+              }}
+              className="pl-3 pr-4 py-2 text-sm rounded-full inline-flex items-center bg-neutral-200 dark:bg-neutral-800 dark:text-white hover:bg-neutral-300 dark:hover:bg-neutral-700 hover:cursor-pointer"
+            >
+              <MagnifyingGlassIcon className="w-4 h-4 inline-block mr-2" />
+              Search Result
+            </button>
+          </div>
         </div>
       </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
     </div>
   );
 }
